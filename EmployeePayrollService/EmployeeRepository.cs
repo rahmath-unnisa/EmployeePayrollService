954ab67907cf4655e041fbb9f62a92bfbd000238
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-06-12 18:06:34 +0530

    [Rahmath]Add: Connecting To The Data Base

---
+++ EmployeePayrollService/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService
+{
+    public class EmployeeRepository
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string connectingString = "Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=payroll_service";
+            con = new SqlConnection(connectingString);
+        }
+    }
+}
